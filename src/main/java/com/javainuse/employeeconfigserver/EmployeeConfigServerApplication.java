ce26d474a4134080bb57639a89c1d46d18c2b2db
Fayaz Shaik <FSHAIK@amdocs.com>
2018-03-22 13:29:44 -0600

    property change

--- src/main/java/com/javainuse/employeeconfigserver/EmployeeConfigServerApplication.java
+++ src/main/java/com/javainuse/employeeconfigserver/EmployeeConfigServerApplication.java
@@ -3,9 +3,12 @@ package com.javainuse.employeeconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableAutoConfiguration
 public class EmployeeConfigServerApplication {
 
 	public static void main(String[] args) {
