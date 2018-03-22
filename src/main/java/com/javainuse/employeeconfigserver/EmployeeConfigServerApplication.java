508e3aa4187ca8bf876cd8692ab471b7922cecc9
Fayaz Shaik <FSHAIK@amdocs.com>
2018-03-22 13:40:52 -0600

    aaa

--- src/main/java/com/javainuse/employeeconfigserver/EmployeeConfigServerApplication.java
+++ src/main/java/com/javainuse/employeeconfigserver/EmployeeConfigServerApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
 @SpringBootApplication
 @EnableConfigServer
 @EnableAutoConfiguration
 public class EmployeeConfigServerApplication {
+	
+	@GetMapping("/hi")
+	String hi() {
+		return "Hello " + "Fayaz Sir";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeConfigServerApplication.class, args);
